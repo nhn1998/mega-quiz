e35361f14e8593a340441b91801b19c958403557
nhn1998 <nhnayeem1@gmail.com>
2022-10-16 16:21:42 +0600

    chart done

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 
-import StatisticsTotal from '../statsticsTotal/StatisticsTotal';
 
 const Statistics = () => {
   const values = useLoaderData()
-  console.log(values)
+  const data = values.data;
+  console.log(data)
   return (
     <div>
-      {
-        values.data.map(value =><StatisticsTotal key={value.id} value={value}></StatisticsTotal>)
-      }
+      <div>
+        <h2 className='text-info mb-5'>This is the Statstics of total quiz</h2>
+      </div>
+      <div className=''>
+      
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend/>
+          <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+    
+      </div>
     </div>
   );
 };
